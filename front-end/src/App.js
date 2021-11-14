d07f7a961a5a6f198ee294c3ebcb4ce0c4f1e6d1
tony onserio <onseriocodes@gmail.com>
2021-11-14 13:59:34 -0600

    Initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Naija Plates</h1>
+      <h3>West African Family Meal</h3>
+    </>
+  );
+};
+
+export default App;
